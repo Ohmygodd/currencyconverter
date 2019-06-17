afd01d03f02af9f1adb13504c4bc29532b4443a0
Ohmygodd <samuelsuhonto@yahoo.com>
2019-06-17 21:10:23 +0700

    Add files via upload

--- src/components/currency.vue
+++ src/components/currency.vue
@@ -8,6 +8,7 @@
         <p>USD</p>
       </div>
       <div class="col-lg-6 col-6">
+        <!-- ammount input -->
         <input id="currVal" type="number" name="" value="10.00" v-on:change="calculate()">
       </div>
     </div>
@@ -19,20 +20,25 @@
               <div id="left" class="container col-lg-10 col-10">
                 <div class="row">
                   <div class="col-lg-6 col-6">
+                    <!-- currency naem -->
                     <p>{{ x.name }}</p>
                   </div>
                   <div class="col-lg-6 col-6">
+                    <!-- converted currency amount -->
                     <p><span class="finalamount">{{ x.total }}</span></p>
                   </div>
                   <div class="col-lg-12 col-12">
+                    <!-- currency -->
                     <p>{{ x.name }} - {{ x.currlong }}</p>
                   </div>
                   <div class="col-lg-12 col-12">
+                    <!-- currency exchange rate -->
                     <p>1 USD = {{ x.name }} {{ x.exRate }}</p>
                   </div>
                 </div>
               </div>
               <div id="del" class="col-lg-2 col-2">
+                <!-- delete currency list -->
                 <a v-on:click="removeElement(index)">(-)</a>
               </div>
             </div>
@@ -42,12 +48,14 @@
       <ul id="add" class="add">
         <li class="loading" style="display: none;">load currency rate...</li>
         <li id="plus" class="active">
+          <!-- addnew currency -->
           <a v-on:click="addCurr = !addCurr">(+) Add More Currencies</a>
         </li>
         <li id="drop" class="dropdown" v-show="addCurr">
           <div class="container">
             <div class="row">
               <div class="col-lg-9 col-9">
+                <!-- currency avaiable list -->
                  <select id="slccurr" v-on:change="othercurr($event)">
                   <option readonly>Choose Currency</option>
                   <option id="optCAD" value="CAD">CAD</option>
@@ -80,21 +88,23 @@
     data(){
       return{
         currency: '',
-        addCurr : true,
+        addCurr : false,
         rate: [
         ]
       }
     },
     methods: {
       calculate(){
+        // triggger re-calculate coversion amount
         this.rates("",1);
       },
       removeElement: function (index) {
+        // remove currency from list
         var name = this.rate[index].name;
         this.$delete(this.rate, index);
         this.$delete(countryarr, index);
-        console.log(countryarr)
         
+        // add deleted currency back to options
         var sel = document.getElementById('slccurr');
         var opt = document.createElement('option');
         opt.appendChild(document.createTextNode(name));
@@ -102,23 +112,24 @@
         opt.value = name;
         sel.appendChild(opt);
       },
-      calculate(){
-        this.rates("",1);
-      },
       othercurr(event) {
+        // calculate newly add currency from option
         var value = event.target.value;
         document.getElementById("opt"+value).remove();
         this.rates(value,0);
       },
       rates: function(curr,x) {
         if (curr!="") {
+          // save coverted currency name 
           countryarr.push(curr);
         }
         var amount = x;
         if (amount > 0) {
+          // base amount change, recalculate converted amount
           this.result("",1);
         }else{
           if (curr == 'CAD' || curr == 'IDR' || curr == 'GBP' || curr == 'CHF' || curr == 'SGD' || curr == 'INR' || curr == 'MYR' || curr == 'JPY' || curr == 'KRW') {
+            // get currency name
             if (curr == 'CAD') {
               currtxt="Canadian Dollar";
             }else if (curr == 'IDR'){
@@ -141,29 +152,35 @@
               currtxt="No Currencies";
             }
             $('.loading').css({'display':'block'});
+            // get exchange rate
             axios.get("https://api.exchangeratesapi.io/latest?base=USD&symbols="+curr)
               .then(response => {
+                // sucess
                 var html='';
                 var rate = response.data.rates;
                 var exRate = rate[curr];
+                // push response to array
                 this.rate.push(
                   {name: curr, exRate: exRate, currlong:currtxt, class:'li'+curr}
                 );
+                // calculate amount
                 this.result(exRate,0);
                 $('.loading').css({'display':'none'});
               })
+              // catch error
               .catch((error) => console.log(error));
           }
         }
       },
       result: function (x,y) {
+        // calculate amount base on exchange rate
         $('.loading').css({'display':'block'});
         var base_amount = $('#currVal').val();
-    console.log("Calc Result")
-    console.log(this.rate)
-        if (y == 0) {
+        
+        if (y == 0) {// calculate amount from rates method
           var index = this.rate.length - 1;
 
+          // remake array
           var final = base_amount*x.toFixed(4);
           var name = this.rate[index].name;
           var exRate = this.rate[index].exRate;
@@ -174,14 +191,13 @@
           this.rate.push(
             {name: name, exRate: exRate.toFixed(4), currlong:currlong, class:c, total:final.toFixed(4)}
           );
-        }else{
+        }else{// if base amount changed amount calculate here
+
+          // get list of selected currency
           var c = countryarr.toString();
-          console.log("arr length")
-          console.log(countryarr.length)
-          console.log(this.rate.length)
-          console.log(this.rate)
           axios.get("https://api.exchangeratesapi.io/latest?base=USD&symbols="+c)
             .then(response => {
+              //loop all selected currency
               for (var i=countryarr.length-1; i>=0; i--) {
                 var final="";
                 var name="";
@@ -189,8 +205,7 @@
                 var c="";
                 var currlong="";
 
-    console.log("Calc change Result "+[i])
-    console.log(this.rate[i])
+                // remake array
                 var rate = response.data.rates;
                 var exRate = rate[countryarr[i]];
                 final = base_amount*exRate.toFixed(4);
@@ -210,7 +225,6 @@
   }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   #box {
     width: 450px;
@@ -240,13 +254,16 @@
   #del a {
     cursor: pointer;
     line-height: 102px;}
+  #plus a {
+    cursor: pointer;}
   #drop .row div{
     padding: 0;}
   #slccurr,
   #calc {
     width: 100%;
     height: 100%;
-    border: none;}
+    border: none;
+    cursor: pointer;}
   #currVal {
     width: 100%;}
   @media only screen and (max-width: 768px) {
